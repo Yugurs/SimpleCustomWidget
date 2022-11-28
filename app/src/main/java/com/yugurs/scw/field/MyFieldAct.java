45e50bab57aa06759f6a24b94a59d228d968dd22
Yugurs <xiong2yq@163.com>
2022-11-28 15:30:21 +0800

    fix

--- app/src/main/java/com/yugurs/scw/field/MyFieldAct.java
+++ app/src/main/java/com/yugurs/scw/field/MyFieldAct.java
@@ -1,10 +1,8 @@
 package com.yugurs.scw.field;
 
 import android.os.Bundle;
-
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.yugurs.scw.databinding.MyfieldActBinding;
 
 public class MyFieldAct extends AppCompatActivity {
